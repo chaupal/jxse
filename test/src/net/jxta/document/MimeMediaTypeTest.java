eaef1f32b173a3574858129d9e1e1de6070cfc66
Mike Duigou <bondolo@dev.java.net>
2007-08-25 01:33:06 +0000

    Adds a test for intern() contention

--- test/src/net/jxta/document/MimeMediaTypeTest.java
+++ test/src/net/jxta/document/MimeMediaTypeTest.java
@@ -1,57 +1,57 @@
 /*
  * Copyright (c) 2001-2007 Sun Microsystems, Inc.  All rights reserved.
- *  
+ *
  *  The Sun Project JXTA(TM) Software License
- *  
- *  Redistribution and use in source and binary forms, with or without 
+ *
+ *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
- *  
+ *
  *  1. Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimer.
- *  
- *  2. Redistributions in binary form must reproduce the above copyright notice, 
- *     this list of conditions and the following disclaimer in the documentation 
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
  *     and/or other materials provided with the distribution.
- *  
- *  3. The end-user documentation included with the redistribution, if any, must 
- *     include the following acknowledgment: "This product includes software 
- *     developed by Sun Microsystems, Inc. for JXTA(TM) technology." 
- *     Alternately, this acknowledgment may appear in the software itself, if 
+ *
+ *  3. The end-user documentation included with the redistribution, if any, must
+ *     include the following acknowledgment: "This product includes software
+ *     developed by Sun Microsystems, Inc. for JXTA(TM) technology."
+ *     Alternately, this acknowledgment may appear in the software itself, if
  *     and wherever such third-party acknowledgments normally appear.
- *  
- *  4. The names "Sun", "Sun Microsystems, Inc.", "JXTA" and "Project JXTA" must 
- *     not be used to endorse or promote products derived from this software 
- *     without prior written permission. For written permission, please contact 
+ *
+ *  4. The names "Sun", "Sun Microsystems, Inc.", "JXTA" and "Project JXTA" must
+ *     not be used to endorse or promote products derived from this software
+ *     without prior written permission. For written permission, please contact
  *     Project JXTA at http://www.jxta.org.
- *  
- *  5. Products derived from this software may not be called "JXTA", nor may 
+ *
+ *  5. Products derived from this software may not be called "JXTA", nor may
  *     "JXTA" appear in their name, without prior written permission of Sun.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SUN 
- *  MICROSYSTEMS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
- *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SUN
+ *  MICROSYSTEMS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *  
- *  JXTA is a registered trademark of Sun Microsystems, Inc. in the United 
+ *
+ *  JXTA is a registered trademark of Sun Microsystems, Inc. in the United
  *  States and other countries.
- *  
+ *
  *  Please see the license information page at :
- *  <http://www.jxta.org/project/www/license.html> for instructions on use of 
+ *  <http://www.jxta.org/project/www/license.html> for instructions on use of
  *  the license in source files.
- *  
+ *
  *  ====================================================================
- *  
- *  This software consists of voluntary contributions made by many individuals 
- *  on behalf of Project JXTA. For more information on Project JXTA, please see 
+ *
+ *  This software consists of voluntary contributions made by many individuals
+ *  on behalf of Project JXTA. For more information on Project JXTA, please see
  *  http://www.jxta.org.
- *  
- *  This license is based on the BSD license adopted by the Apache Foundation. 
+ *
+ *  This license is based on the BSD license adopted by the Apache Foundation.
  */
 package net.jxta.document;
 
@@ -62,6 +62,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import junit.framework.*;
 
@@ -123,7 +126,7 @@ public class MimeMediaTypeTest extends TestCase {
         
         MimeMediaType experimentalXMLString = new MimeMediaType("x-text", "x-xml");
         MimeMediaType nonexperimentalXMLString = new MimeMediaType("text", "xml");
-
+        
         assertTrue(experimentalXMLString.isExperimentalType());
         assertTrue(experimentalXMLString.isExperimentalSubtype());
         assertTrue(!nonexperimentalXMLString.isExperimentalType());
@@ -141,7 +144,7 @@ public class MimeMediaTypeTest extends TestCase {
         
         MimeMediaType roundtrippedmany = new MimeMediaType(xmlmanyparamsstring.toString());
         
-        assertTrue(roundtrippedmany.equals(xmlmanyparamsstring));       
+        assertTrue(roundtrippedmany.equals(xmlmanyparamsstring));
         
         MimeMediaType xmllowercharset = new MimeMediaType("text/xml;charset=\"UTF-8\"");
         MimeMediaType xmlUPPERcharset = new MimeMediaType("text/xml;CHARSET=\"UTF-8\"");
@@ -170,7 +173,7 @@ public class MimeMediaTypeTest extends TestCase {
         assertTrue(newed.equals(valued));
         MimeMediaType newInterned = newed.intern();
         MimeMediaType valueInterned = valued.intern();
-
+        
         assertTrue(valued == valueInterned);
         
         assertTrue(newInterned.equals(valueInterned));
@@ -178,7 +181,7 @@ public class MimeMediaTypeTest extends TestCase {
     }
     
     public void testInternPerformance() {
-        int trials = 200000;
+        int trials = 10000;
         String [] strings = new String[trials];
         MimeMediaType [] mimes = new MimeMediaType[trials];
         
@@ -193,7 +196,7 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]);
         }
@@ -204,7 +207,7 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]).intern();
         }
@@ -213,7 +216,7 @@ public class MimeMediaTypeTest extends TestCase {
         
         Arrays.fill(strings, null);
         Arrays.fill(mimes, null);
-                
+        
         for(int each = 0; each < trials; each++) {
             strings[each] = new String("text/xml; charset=\"UTF-XX" + each + "\"");
         }
@@ -222,7 +225,7 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]);
         }
@@ -233,17 +236,17 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]).intern();
         }
         stop = System.currentTimeMillis();
         System.out.println( stop - start + "ms for " + trials + " unique interned types.");
-                
-                
+        
+        
         Arrays.fill(strings, null);
         Arrays.fill(mimes, null);
-
+        
         for(int each = 0; each < trials; each++) {
             strings[each] = new String("text/xml; charset=\"UTF-XX" + (each % 100) + "\"");
         }
@@ -252,7 +255,7 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]);
         }
@@ -263,14 +266,75 @@ public class MimeMediaTypeTest extends TestCase {
         System.gc();
         System.gc();
         
-        start = System.currentTimeMillis();        
+        start = System.currentTimeMillis();
         for(int each = 0; each < trials; each++) {
             mimes[each] = new MimeMediaType(strings[each]).intern();
         }
         stop = System.currentTimeMillis();
         System.out.println( stop - start + "ms for " + trials + " non-unique interned types.");
     }
+    
+    public void testInternContention() {
+        final int concurrency = 50;
+        final int trials = 50000;
+        long stop;
+        
+        ExecutorService executor = Executors.newCachedThreadPool();
+        
+        // Pre-create threads.
+        for(int spawn = 0; spawn < concurrency; spawn++) {
+            executor.execute( new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep(500);
+                    } catch(InterruptedException woken) {
+                        
+                    }
+                }
+            });
+        }
+        
+        try {
+            Thread.sleep(500);
+        } catch(InterruptedException woken) {
             
+        }
+        
+        System.runFinalization();
+        System.gc();
+        System.gc();
+        
+        final long start = System.currentTimeMillis();
+        for(int each = 0; each < concurrency; each++) {
+            final String runcount = "Run #" + each;
+            executor.execute( new Runnable() {
+                public void run() {
+                    System.err.println( runcount + " start " + (System.currentTimeMillis() - start));
+                    
+                    MimeMediaType [] mimes = new MimeMediaType[trials];
+                    
+                    for(int each = 0; each < trials; each++) {
+                        mimes[each] = new MimeMediaType(new String("text/xml")).intern();
+                    }
+                    
+                    System.err.println( runcount + " done " + (System.currentTimeMillis() - start));
+                }
+            });
+        }
+       
+        executor.shutdown();
+        try {
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        } catch(InterruptedException woken) {
+            
+        }
+        stop = System.currentTimeMillis();
+        System.err.flush();
+        System.out.flush();
+        
+        System.out.println( stop - start + "ms for " + trials + " identical types on " + concurrency + " threads.");        
+    }
+    
     public void testSerialization() {
         MimeMediaType one = new MimeMediaType("text/xml");
         
@@ -297,7 +361,7 @@ public class MimeMediaTypeTest extends TestCase {
     
     public static Test suite() {
         TestSuite suite = new TestSuite(MimeMediaTypeTest.class);
-
+        
         return suite;
     }
     
