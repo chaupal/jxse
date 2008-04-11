f5d7c5798cdc7f1f757010af96b38dafa4c93170
Mohamed Abdelaziz <Mohamed.Abdelaziz@Sun.COM>
2008-04-11 23:07:21 +0000

    Improves handling of underlaying messenger creation (provided by bondolo and hamada)

--- impl/src/net/jxta/impl/pipe/BlockingWireOutputPipe.java
+++ impl/src/net/jxta/impl/pipe/BlockingWireOutputPipe.java
@@ -1,57 +1,56 @@
 /*
  * Copyright (c) 2001-2007 Sun Microsystems, Inc.  All rights reserved.
- *  
+ *
  *  The Sun Project JXTA(TM) Software License
- *  
- *  Redistribution and use in source and binary forms, with or without 
+ *
+ *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
- *  
+ *
  *  1. Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimer.
- *  
- *  2. Redistributions in binary form must reproduce the above copyright notice, 
- *     this list of conditions and the following disclaimer in the documentation 
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
  *     and/or other materials provided with the distribution.
- *  
- *  3. The end-user documentation included with the redistribution, if any, must 
- *     include the following acknowledgment: "This product includes software 
- *     developed by Sun Microsystems, Inc. for JXTA(TM) technology." 
- *     Alternately, this acknowledgment may appear in the software itself, if 
+ *
+ *  3. The end-user documentation included with the redistribution, if any, must
+ *     include the following acknowledgment: "This product includes software
+ *     developed by Sun Microsystems, Inc. for JXTA(TM) technology."
+ *     Alternately, this acknowledgment may appear in the software itself, if
  *     and wherever such third-party acknowledgments normally appear.
- *  
- *  4. The names "Sun", "Sun Microsystems, Inc.", "JXTA" and "Project JXTA" must 
- *     not be used to endorse or promote products derived from this software 
- *     without prior written permission. For written permission, please contact 
+ *
+ *  4. The names "Sun", "Sun Microsystems, Inc.", "JXTA" and "Project JXTA" must
+ *     not be used to endorse or promote products derived from this software
+ *     without prior written permission. For written permission, please contact
  *     Project JXTA at http://www.jxta.org.
- *  
- *  5. Products derived from this software may not be called "JXTA", nor may 
+ *
+ *  5. Products derived from this software may not be called "JXTA", nor may
  *     "JXTA" appear in their name, without prior written permission of Sun.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SUN 
- *  MICROSYSTEMS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
- *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SUN
+ *  MICROSYSTEMS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ *  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *  
- *  JXTA is a registered trademark of Sun Microsystems, Inc. in the United 
+ *
+ *  JXTA is a registered trademark of Sun Microsystems, Inc. in the United
  *  States and other countries.
- *  
+ *
  *  Please see the license information page at :
- *  <http://www.jxta.org/project/www/license.html> for instructions on use of 
+ *  <http://www.jxta.org/project/www/license.html> for instructions on use of
  *  the license in source files.
- *  
- *  ====================================================================
- *  
- *  This software consists of voluntary contributions made by many individuals 
- *  on behalf of Project JXTA. For more information on Project JXTA, please see 
+ *
+ *
+ *  This software consists of voluntary contributions made by many individuals
+ *  on behalf of Project JXTA. For more information on Project JXTA, please see
  *  http://www.jxta.org.
- *  
- *  This license is based on the BSD license adopted by the Apache Foundation. 
+ *
+ *  This license is based on the BSD license adopted by the Apache Foundation.
  */
 package net.jxta.impl.pipe;
 
@@ -64,7 +63,6 @@ import net.jxta.endpoint.MessageElement;
 import net.jxta.endpoint.Messenger;
 import net.jxta.endpoint.TextDocumentMessageElement;
 import net.jxta.id.ID;
-import net.jxta.impl.endpoint.tcp.TcpMessenger;
 import net.jxta.logging.Logging;
 import net.jxta.peer.PeerID;
 import net.jxta.peergroup.PeerGroup;
@@ -77,12 +75,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This Object is created when a call to PipeService.createOutputPipe(propgateAdv) with a Set containing a single
- * PeerID.  This pipe blocks until a valid messeger is created (i.e. resolved and useable). With this object it is
- * possible to detect connection failures during the messenger resolution. Notge, this pipe also avoids utilitizing the
- * rendezvous for propagation, effectively reducing message overhead, resulting in improved performance.
- * <p/>
- * #send is remains asynchronous.
+ * Created when a call to {@code PipeService.createOutputPipe(propgateAdv)} with
+ * a {@code Set} containing a single PeerID. This pipe blocks until a valid
+ * messenger is created (i.e. resolved and useable). With this object it is
+ * possible to detect connection failures during the messenger resolution. Note,
+ * this pipe also avoids utilitizing the rendezvous for propagation, effectively
+ * reducing message overhead, resulting in improved performance.
  */
 public class BlockingWireOutputPipe implements OutputPipe {
 
@@ -101,11 +99,29 @@ public class BlockingWireOutputPipe implements OutputPipe {
      */
     private final PipeAdvertisement pAdv;
 
+    /**
+     * The Peer Group in which this pipe is being used.
+     */
     private final PeerGroup group;
+
+    /**
+     * Endpoint service in the host peer group.
+     */
+    private final EndpointService endpoint;
+
+    private final EndpointAddress destination;
+
+    /**
+     * The route we use for the destination peer.
+     * <p/>
+     * We don't provide any provision for the route changing.
+     */
+    private final RouteAdvertisement route;
+
+    /**
+     * The messenger to the destination peer.
+     */
     private Messenger destMessenger = null;
-    private EndpointAddress destination;
-    private EndpointService endpoint = null;
-    private RouteAdvertisement route = null;
 
     /**
      * Create a new blocking output pipe
@@ -113,39 +129,30 @@ public class BlockingWireOutputPipe implements OutputPipe {
      * @param group  The peergroup context.
      * @param pAdv   advertisement for the pipe we are supporting.
      * @param peerID the destination <code>PeerID</code>.
+     * @throws IOException for failures creating a pipe to the destination peer.
      */
-    public BlockingWireOutputPipe(PeerGroup group, PipeAdvertisement pAdv, PeerID peerID) {
-
-        this.pAdv = pAdv;
-        this.group = group;
-        this.endpoint = group.getEndpointService();
-        destination = new EndpointAddress("jxta", peerID.getUniqueValue().toString(), "PipeService", pAdv.getID().toString());
-        destMessenger = endpoint.getMessenger(destination);
-
-        if (Logging.SHOW_INFO && LOG.isLoggable(Level.INFO)) {
-            LOG.info("Created output pipe for " + getPipeID());
-        }
+    public BlockingWireOutputPipe(PeerGroup group, PipeAdvertisement pAdv, PeerID peerID) throws IOException {
+        this(group, pAdv, peerID, null);
     }
+
     /**
      * Create a new blocking output pipe
      *
      * @param group  The peergroup context.
      * @param pAdv   advertisement for the pipe we are supporting.
      * @param peerID the destination <code>PeerID</code>.
-     * @param route the destination route.
+     * @param route  the destination route.
+     * @throws IOException for failures creating a pipe to the destination peer.
      */
-    public BlockingWireOutputPipe(PeerGroup group, PipeAdvertisement pAdv, PeerID peerID, RouteAdvertisement route) {
-        this.route = route;
+    public BlockingWireOutputPipe(PeerGroup group, PipeAdvertisement pAdv, PeerID peerID, RouteAdvertisement route) throws IOException {
         this.pAdv = pAdv;
         this.group = group;
         this.endpoint = group.getEndpointService();
         destination = new EndpointAddress("jxta", peerID.getUniqueValue().toString(), "PipeService", pAdv.getID().toString());
-        if (route != null) {
-            destMessenger = endpoint.getDirectMessenger(destination, route, true);
-        }
-        if (destMessenger == null) {
-            destMessenger = endpoint.getMessenger(destination);
-        }
+        this.route = route;
+
+        checkMessenger();
+
         if (Logging.SHOW_INFO && LOG.isLoggable(Level.INFO)) {
             LOG.info("Created output pipe for " + getPipeID());
         }
@@ -155,14 +162,14 @@ public class BlockingWireOutputPipe implements OutputPipe {
      * {@inheritDoc}
      */
     public synchronized void close() {
-
         if (closed) {
             return;
         }
-        // Close the queue so that no more messages are accepted
+
         if (Logging.SHOW_INFO && LOG.isLoggable(Level.INFO)) {
-            LOG.info("Closing queue for " + getPipeID());
+            LOG.info("Closing " + getPipeID());
         }
+
         closed = true;
     }
 
@@ -201,30 +208,37 @@ public class BlockingWireOutputPipe implements OutputPipe {
         return pAdv;
     }
 
-    private void checkMessenger() throws IOException {
-        if (!(destMessenger instanceof TcpMessenger) && destMessenger != null && (destMessenger.getState() & Messenger.USABLE) != 0) {
-            return;
+    /**
+     * Check the messenger to the remote peer and attempt to re-create the
+     * messenger if necessary.
+     *
+     * @return {@code true} if the current messenger is adequate.
+     * @throws IOException is thrown for errors in creating a new messenger.
+     */
+    private synchronized boolean checkMessenger() throws IOException {
+        if ((destMessenger != null) && ((destMessenger.getState() & Messenger.USABLE) != 0)) {
+            // Everything fine!
+            return true;
         }
-        if (destMessenger != null && !destMessenger.isClosed()) {
-                return;
+
+        // Try making a direct messenger first.
+        if (route != null) {
+            destMessenger = endpoint.getDirectMessenger(destination, route, true);
         }
-        synchronized (this) {
-            if (route != null) {
-                destMessenger = endpoint.getDirectMessenger(destination, route, true);
-            }
-            if (destMessenger != null && !destMessenger.isClosed()) {
-                //got a direct messenger, return
-                return;
-            }
-            destMessenger = endpoint.getMessenger(destination);
-            if (!(destMessenger instanceof TcpMessenger) && destMessenger == null || (destMessenger.getState() & Messenger.TERMINAL) != 0) {
-                if (destMessenger != null) {
-                    destMessenger.close();
-                    destMessenger = null;
-                }
-                throw new IOException("Unable to create a messenger to " + destination.toString());
-            }
+
+        // Try making a regular messenger if that didn't work.
+        if ((destMessenger == null) || ((destMessenger.getState() & Messenger.TERMINAL) != 0)) {
+            destMessenger = null;
+            destMessenger = endpoint.getMessenger(destination, route);
         }
+
+        // Dismal failure
+        if ((destMessenger == null) || ((destMessenger.getState() & Messenger.TERMINAL) != 0)) {
+            destMessenger = null;
+            throw new IOException("Unable to create a messenger to " + destination.toString());
+        }
+
+        return true;
     }
 
     /**
@@ -234,6 +248,8 @@ public class BlockingWireOutputPipe implements OutputPipe {
         if (closed) {
             throw new IOException("Pipe closed");
         }
+        Message msg = message.clone();
+
         WireHeader header = new WireHeader();
 
         header.setPipeID(getPipeID());
@@ -243,22 +259,15 @@ public class BlockingWireOutputPipe implements OutputPipe {
 
         XMLDocument asDoc = (XMLDocument) header.getDocument(MimeMediaType.XMLUTF8);
         MessageElement elem = new TextDocumentMessageElement(WirePipeImpl.WIRE_HEADER_ELEMENT_NAME, asDoc, null);
-        Message msg = message.clone();
-
         msg.replaceMessageElement(WirePipeImpl.WIRE_HEADER_ELEMENT_NAMESPACE, elem);
+
         checkMessenger();
         try {
-            if (destMessenger instanceof TcpMessenger) {
-                ((TcpMessenger) destMessenger).sendMessageDirect(msg, null, null, true);
-            } else  if (!destMessenger.sendMessage(msg, null, null)) {
-                throw new IOException("Pipe closed");
-            }
+            return destMessenger.sendMessage(msg, null, null);
         } catch (IOException io) {
             checkMessenger();
-            if (!destMessenger.sendMessage(msg, null, null)) {
-                throw new IOException("Pipe closed");
-            }
+            return destMessenger.sendMessage(msg, null, null);
         }
-        return true;
+
     }
 }
