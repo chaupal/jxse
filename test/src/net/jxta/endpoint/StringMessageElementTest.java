1e3164f980eadb00979f915eba96ac87114f96cf
Mike Duigou <bondolo@dev.java.net>
2008-10-07 16:19:20 +0000

    A somewhat more challenging unit test.

--- test/src/net/jxta/endpoint/StringMessageElementTest.java
+++ test/src/net/jxta/endpoint/StringMessageElementTest.java
@@ -53,193 +53,215 @@
  *  
  *  This license is based on the BSD license adopted by the Apache Foundation. 
  */
-
 package net.jxta.endpoint;
 
-
-import java.io.InputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.nio.CharBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import junit.framework.*;
 
 import net.jxta.endpoint.MessageElement;
 import net.jxta.endpoint.StringMessageElement;
-
 
 /**
  *
  * @author mike
  */
 public class StringMessageElementTest extends TestCase {
-    
-    static final String one = "happy\u022a";
-    static final String two = "happy\u0229";
-    static final String three = "happy\u0229";
-    
+
+    static final CharSequence one = "happy\u0221";
+    static final String two = "happy\u0222";
+    static final StringBuffer three = new StringBuffer("happy\u0223");
+    static final StringBuilder four = new StringBuilder("happy\u0224");
+    static final CharBuffer five = CharBuffer.wrap(one);    // message with UTF-8 encoding
+
+    static class ElementTestCase {
+
+        final CharSequence source;
+        final TextMessageElement element;
+
+        ElementTestCase(CharSequence source, TextMessageElement element) {
+            this.source = source;
+            this.element = element;
+        }
+    }
+    final List<ElementTestCase> testElements = new ArrayList<ElementTestCase>();
+
     public StringMessageElementTest(java.lang.String testName) {
         super(testName);
+
+        try {
+            testElements.add(new ElementTestCase(one, new StringMessageElement("element1", one, (MessageElement) null)));
+            testElements.add(new ElementTestCase(two, new StringMessageElement("element2", two, "UTF-16", (MessageElement) null)));
+            testElements.add(new ElementTestCase(three, new StringMessageElement("element3", three, null, (MessageElement) null)));
+            testElements.add(new ElementTestCase(four, new StringMessageElement("element4", four, null, (MessageElement) null)));
+            testElements.add(new ElementTestCase(five, new StringMessageElement("element5", five, null, (MessageElement) null)));
+        } catch (IOException failed) {
+            throw new IllegalStateException("Could not create elements", failed);
+        }
     }
-    
+
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
-        
+
         System.err.flush();
         System.out.flush();
     }
-    
+
     public static Test suite() {
         TestSuite suite = new TestSuite(StringMessageElementTest.class);
 
         return suite;
     }
-    
+
     public void testAccessors() {
         try {
-            // message with UTF-8 encoding
-            TextMessageElement el1 = new StringMessageElement("element1", one, (MessageElement) null);
-            
-            // message with UTF-16 encoding
-            TextMessageElement el2 = new StringMessageElement("element2", two, "UTF-16", (MessageElement) null);
-            
-            // message with system encoding
-            TextMessageElement el3 = new StringMessageElement(null, three, null, (MessageElement) null);
-            
-            // toString.
-            assertTrue(one.equals(el1.toString()));
-            
-            assertTrue(two.equals(el2.toString()));
-            
-            assertTrue(three.equals(el3.toString()));
-            
-            // charLength
-            assertTrue(one.length() == el1.getCharLength());
-            
-            assertTrue(two.length() == el2.getCharLength());
-            
-            assertTrue(three.length() == el3.getCharLength());
-            
-            // getByteLength
-            assertTrue(el1.getByteLength() == el1.getBytes(false).length);
-            
-            assertTrue(el2.getByteLength() == el2.getBytes(false).length);
-            
-            assertTrue(el3.getByteLength() == el3.getBytes(false).length);
+            for (ElementTestCase elem : testElements) {
+                String sourceString = elem.source.toString();
+                String elemString = elem.element.toString();
+
+                assertEquals(sourceString, elemString);
+
+                assertTrue(sourceString.length() == elem.element.getCharLength());
+
+                assertTrue(elem.element.getByteLength() == elemString.getBytes(elem.element.getMimeType().getParameter("charset")).length);
+            }
         } catch (Exception caught) {
             caught.printStackTrace();
             fail("exception thrown : " + caught.getMessage());
         }
     }
-    
-    public void testStreams() {
+
+    public void testEquals() {
         try {
-            // message with UTF-8 encoding
-            TextMessageElement el1 = new StringMessageElement("element1", one, (MessageElement) null);
-            
-            // message with UTF-16 encoding
-            TextMessageElement el2 = new StringMessageElement("element2", two, "UTF-16", (MessageElement) null);
-            
-            // message with system encoding
-            TextMessageElement el3 = new StringMessageElement(null, three, null, (MessageElement) null);
-            
-            // sendDataStream
-            
-            System.out.print("el1 send (" + el1.getElementName() + "," + el1.getMimeType() + ") : ");
-            ByteArrayOutputStream el1stream = new ByteArrayOutputStream();
-            
-            el1.sendToStream(el1stream);
-            
-            el1.sendToStream(System.out);
-            
-            System.out.println();
-            
-            System.out.print("el2 send (" + el2.getElementName() + "," + el2.getMimeType() + ") : ");
-            ByteArrayOutputStream el2stream = new ByteArrayOutputStream();
-            
-            el2.sendToStream(el2stream);
-            
-            el2.sendToStream(System.out);
-            
-            System.out.println();
-            
-            System.out.print("el3 send (" + el3.getElementName() + "," + el3.getMimeType() + ") : ");
-            ByteArrayOutputStream el3stream = new ByteArrayOutputStream();
-            
-            el3.sendToStream(el3stream);
-            
-            el3.sendToStream(System.out);
-            
-            System.out.println();
-            
-            System.out.print("el1 read (" + el1.getElementName() + "," + el1.getMimeType() + ") : ");
-            
-            // getDataStream
-            
-            InputStream in = el1.getStream();
-            byte outputbytes[] = el1stream.toByteArray();
-            int index = 0;
-            
-            do {
-                int aChar = in.read();
+            for (ElementTestCase elem : testElements) {
+                TextMessageElement newElem = new StringMessageElement(elem.element.getElementName(), elem.source.toString(), elem.element.getMimeType().getParameter("charset"), null);
                 
-                if (-1 == aChar) {
-                    break;
+                assertEquals(elem.element, newElem);
+
+                assertEquals(newElem, elem.element);
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testGetBytes() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                byte sourceBytes[] = elem.source.toString().getBytes(elem.element.getMimeType().getParameter("charset"));
+                byte elemBytes[] = elem.element.getBytes(false);
+
+                this.assertTrue("values not equal", Arrays.equals(sourceBytes, elemBytes));
+
+                elemBytes = elem.element.getBytes(true);
+
+                this.assertTrue("values not equal", Arrays.equals(sourceBytes, elemBytes));
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testInputStream() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                byte sourceBytes[] = elem.source.toString().getBytes(elem.element.getMimeType().getParameter("charset"));
+                InputStream is = elem.element.getStream();
+                DataInput dis = new DataInputStream(is);
+                byte streamBytes[] = new byte[sourceBytes.length];
+
+                dis.readFully(streamBytes);
+                try {
+                    dis.readByte();
+                    fail("Not at EOF");
+                } catch (EOFException atEOF) {
                 }
-                
-                assertTrue("characters did not match at index " + index, aChar == (outputbytes[index++] & 0xFF));
-                
-                System.out.print("0x" + Integer.toHexString(aChar) + " ");
-                
-            } while (true);
-            
-            assertTrue(index == outputbytes.length);
-            
-            System.out.println();
-            
-            System.out.print("el2 read (" + el2.getElementName() + "," + el2.getMimeType() + ") : ");
-            
-            in = el2.getStream();
-            outputbytes = el2stream.toByteArray();
-            index = 0;
-            
-            do {
-                int aChar = in.read();
-                
-                if (-1 == aChar) {
-                    break;
-                }
-                
-                assertTrue("characters did not match at index " + index, aChar == (outputbytes[index++] & 0xFF));
-                
-                System.out.print("0x" + Integer.toHexString(aChar) + " ");
-                
-            } while (true);
-            
-            assertTrue(index == outputbytes.length);
-            
-            System.out.println();
-            
-            System.out.print("el3 read (" + el3.getElementName() + "," + el3.getMimeType() + ") : ");
-            
-            in = el3.getStream();
-            outputbytes = el3stream.toByteArray();
-            index = 0;
-            
-            do {
-                int aChar = in.read();
-                
-                if (-1 == aChar) {
-                    break;
-                }
-                
-                assertTrue("characters did not match at index " + index, aChar == (outputbytes[index++] & 0xFF));
-                
-                System.out.print("0x" + Integer.toHexString(aChar) + " ");
-                
-            } while (true);
-            
-            assertTrue(index == outputbytes.length);
-            
-            System.out.println();
-            
+
+                this.assertTrue("values not equal", Arrays.equals(sourceBytes, streamBytes));
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testReader() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                char sourceChars[] = elem.source.toString().toCharArray();
+                Reader reader = new BufferedReader(elem.element.getReader());
+                char readerChars[] = new char[sourceChars.length];
+
+                assertEquals("Incorrect Number of chars", reader.read(readerChars), sourceChars.length);
+
+                assertEquals("Not at EOF", -1, reader.read());
+
+                assertTrue("values not equal", Arrays.equals(sourceChars, readerChars));
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testGetChars() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                char sourceChars[] = elem.source.toString().toCharArray();
+                char elemChars[] = elem.element.getChars(false);
+
+                this.assertTrue("values not equal", Arrays.equals(sourceChars, elemChars));
+
+                elemChars = elem.element.getChars(true);
+
+                assertTrue("values not equal", Arrays.equals(sourceChars, elemChars));
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testSendToStream() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                byte sourceBytes[] = elem.source.toString().getBytes(elem.element.getMimeType().getParameter("charset"));
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                elem.element.sendToStream(baos);
+                baos.close();
+
+                assertTrue("values not equal", Arrays.equals(sourceBytes, baos.toByteArray()));
+            }
+        } catch (Exception caught) {
+            caught.printStackTrace();
+            fail("exception thrown : " + caught.getMessage());
+        }
+    }
+
+    public void testSendToWriter() {
+        try {
+            for (ElementTestCase elem : testElements) {
+                StringWriter sw = new StringWriter();
+
+                elem.element.sendToWriter(sw);
+                sw.close();
+
+                this.assertEquals("values not equal", elem.source.toString(), sw.toString());
+            }
         } catch (Exception caught) {
             caught.printStackTrace();
             fail("exception thrown : " + caught.getMessage());
