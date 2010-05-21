d6ee164ea85919bfe1d57229b579adf4bcbe19ed
Jérôme Verstrynge <JVerstry@gmail.com>
2010-05-21 12:53:12 +0000

    Correcting small compilation issue of test classes

--- test/src/net/jxta/impl/content/AbstractContentProviderTest.java
+++ test/src/net/jxta/impl/content/AbstractContentProviderTest.java
@@ -144,7 +144,7 @@ public abstract class AbstractContentProviderTest {
         
         public void init() {
             try {
-                LOG.info("Initializing: ", this);
+                LOG.info("Initializing: " + this);
                 PeerGroup netPeerGroup = nm.startNetwork();
                 nm.waitForRendezvousConnection(15000);
                 LOG.info("Am  RDV? " + netPeerGroup.isRendezvous());
@@ -206,7 +206,7 @@ public abstract class AbstractContentProviderTest {
         }
 
         public void destroy() {
-            LOG.info("Destroying: ", this);
+            LOG.info("Destroying: " + this);
             nm.stopNetwork();
             nm = null;
             service = null;
